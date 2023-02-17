74d90968ec37c3c80478793a926260274ba20a49
enzo:) <enzo.comperat@gmail.com>
2023-02-17 10:46:30 +0100

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigurationService } from './configuration/configuration/configuration.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigurationService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+
+  constructor(private readonly configurationService: ConfigurationService){
+    AppModule.port = this.configurationService.port as number;
+  }
+}

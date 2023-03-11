40b46e375e8f72d3f3f6a234f5a9be17e055f337
Enzo Comperat <enzocomperat@MacBook-Air-de-Enzo.local>
2023-03-12 00:48:03 +0100

    Integration typeorm, mapping table users

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}

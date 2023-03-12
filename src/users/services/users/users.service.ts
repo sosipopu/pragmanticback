f83cde4e90613517ca4bbdeb64793dc7bd036e1c
Enzo <45255580+sosipopu@users.noreply.github.com>
2023-03-12 21:39:57 +0100

    Update Delete User

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
-import { CreateUserParams } from 'src/utils/types';
+import { CreateUserParams, UpdateUserParams } from 'src/utils/types';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,9 +9,21 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
-  findUsers() {}
+
+  findUsers() {
+    return this.userRepository.find();
+  }
+
   createUsers(userDetails: CreateUserParams) {
     const newUser = this.userRepository.create({ ...userDetails });
     return this.userRepository.save(newUser);
+  }
+
+  updateUser(id: number, updateUserDetails: UpdateUserParams) {
+    return this.userRepository.update({ id }, { ...updateUserDetails });
+  }
+
+  deleteUser(id: number) {
+    return this.userRepository.delete({ id });
   }
 }

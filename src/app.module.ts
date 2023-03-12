61c51d22054965e16c37e7bb8cedd0ae1da59b00
Enzo <45255580+sosipopu@users.noreply.github.com>
2023-03-12 20:37:54 +0100

    Integration typeorm, mapping table users, endpoint createuser, integration swagger (#4)
    * Integration typeorm, mapping table users
    
    * createUser
    
    * integration swagger
    
    ---------
    
    Co-authored-by: Enzo Comperat <enzocomperat@MacBook-Air-de-Enzo.local>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
+import { type, host, port, username, password, database } from './mysqlcred';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: type,
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+      database: database,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

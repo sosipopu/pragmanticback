f1472a987fcd0c0d0caea0c8ae9b0149f5274cd2
Enzo <45255580+sosipopu@users.noreply.github.com>
2023-03-16 18:53:59 +0100

    amelioration docu swagger et protection de toutes les routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+<<<<<<< Updated upstream
+=======
 import { AuthenticatedGuard } from './auth/authenticated.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/services/auth.service';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+>>>>>>> Stashed changes
 
+@ApiBearerAuth()
 @Controller()
+@ApiTags('App')
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
@@ -15,9 +21,23 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
+<<<<<<< Updated upstream
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+=======
+  @ApiOperation({ summary: 'Auth with email and password' })
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return this.authService.login(req.user);
+  }
+
+  @ApiOperation({ summary: 'Protected test route' })
   @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHello(@Request() req): string {
     return req.user;
+>>>>>>> Stashed changes
   }
 }
--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -7,26 +7,35 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@ApiBearerAuth()
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Get()
+  @UseGuards(JwtAuthGuard)
   getUsers() {
     return this.userService.findUsers();
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   createUser(@Body() createUserDto: CreateUserDto) {
     const { ...userDetails } = createUserDto;
     this.userService.createUsers(userDetails);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async updateUserById(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -35,12 +44,17 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   deleteUserById(@Param('id', ParseIntPipe) id: number) {
     this.userService.deleteUser(id);
   }
+<<<<<<< Updated upstream
+=======
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   getOneUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findUserById(id);
   }
+>>>>>>> Stashed changes
 }

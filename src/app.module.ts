2c07b995347c3a1d4820ece3408fb625e1502cec
sosipopu <enzo.comperat@gmail.com>
2023-03-18 11:59:45 +0100

    adding type orm config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,30 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
-import { type, host, port, username, password, database } from './mysqlcred';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: './.development.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: parseInt(configService.get<string>('DATABASE_PORT')),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASS'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [UserEntity],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     UsersModule,
     AuthModule,
   ],
